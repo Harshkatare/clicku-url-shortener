5e9985af13ba39c25b733fcf192c321e10f3f99a
Haarsh Wintel <harshkatare977@gmail.com>
2026-05-21 23:57:06 +0530

    feat(url): add URL deletion controller

--- backend/src/modules/url/url.controller.ts
+++ backend/src/modules/url/url.controller.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from "express";
 
 import * as urlService from "./url.service.js";
-
 import { createUrlSchema } from "./url.schema.js";
-import { success } from "zod";
 
 export async function createShortUrl(
   req: Request,
@@ -28,7 +26,8 @@ export async function redirectToOriginalUrl(
   req: Request,
   res: Response
 ) {
-  const shortCode = req.params.shortCode as string;
+  const shortCode =
+    req.params.shortCode as string;
 
   const originalUrl =
     await urlService.redirectToOriginalUrl(
@@ -42,12 +41,29 @@ export async function getUserUrls(
   req: Request,
   res: Response
 ) {
-  const userUrls = await urlService.getUserUrls(
-    req.user!.id
-  );
+  const userUrls =
+    await urlService.getUserUrls(
+      req.user!.id
+    );
 
   res.status(200).json({
     success: true,
     data: userUrls,
+  });
+}
+
+export async function deleteUrl(
+  req: Request,
+  res: Response
+) {
+  const deletedUrl =
+    await urlService.deleteUrl(
+      req.params.id as string,
+      req.user!.id
+    );
+
+  res.status(200).json({
+    success: true,
+    data: deletedUrl,
   });
 }
